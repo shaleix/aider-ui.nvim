5d694a922c0f7cf8242ae5bc0aead2def0f7eb96
shalei <shalei6698@126.com>
2024-12-03 21:23:19 +0800

    feat: Add session_status function and setup tracking

--- lua/aider-ui/init.lua
+++ lua/aider-ui/init.lua
@@ -1,10 +1,22 @@
 local M = {}
-local config = require("aider-ui.config")
-local command = require("aider-ui.command")
 
-M.setup = function (opts)
-    config.setup(opts)
-    command.setup()
+local _setup = false
+
+M.setup = function(opts)
+  local config = require("aider-ui.config")
+  local command = require("aider-ui.command")
+  config.setup(opts)
+  command.setup()
+  _setup = true
+end
+
+-- list of session info: { name: str, processing: bool, is_current: bool, need_confirm: bool}
+M.session_status = function()
+  if not _setup then
+    return {}
+  end
+  local aider_sessions = require("aider-ui.aider_sessions_manager")
+  return aider_sessions.list_session_status()
 end
 
 return M
